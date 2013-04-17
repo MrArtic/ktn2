e40fb344f52debbf092922b14c7f476cf29cb996
Jens Kristian Espevik <jenskristian@gmail.com>
2013-04-17 15:33:13 +0200

    added send() once again after it went somewhere else

--- src/no/ntnu/fp/net/co/ConnectionImpl.java
+++ src/no/ntnu/fp/net/co/ConnectionImpl.java
@@ -138,7 +138,19 @@ public class ConnectionImpl extends AbstractConnection {
 		state = State.SYN_RCVD;
 
 		ConnectionImpl newConnection = new ConnectionImpl(this.myAddress, getNewPort(), this.remoteAddress, this.remotePort);
-
+		
+		newConnection.sendAck(syn, true);
+		
+		KtnDatagram ack = newConnection.receiveAck();
+		
+		if(ack==null || ack.getFlag() != Flag.ACK){
+			throw new SocketTimeoutException();
+		}
+		
+		newConnection.state = State.ESTABLISHED;
+		
+		state = State.LISTEN;
+		
 		return (Connection) newConnection;
 	}
 
@@ -159,8 +171,41 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see no.ntnu.fp.net.co.Connection#send(String)
 	 */
 	public void send(String msg) throws ConnectException, IOException {
-		throw new NotImplementedException();
-	}
+//      throw new NotImplementedException();
+  	if(state!= State.ESTABLISHED) throw new ConnectException("No connection established!");
+  	KtnDatagram datagram = constructDataPacket(msg);
+  	
+  	KtnDatagram ack = sendDataPacketWithRetransmit(datagram);//sends msg, waits for ack
+  	System.out.println("send complete");
+  	if(ack == null){
+  		System.out.println("Where's the ack?");
+  		if(resends < maxresends){
+  			resends++;
+  			send(msg);
+  			resends = 0;
+  			return;
+  		} else {
+  			state = State.CLOSED;
+  			throw new ConnectException("Conncetion lost");
+  			
+  		}
+  		
+  	} else { //if ack showed up
+  		System.out.println("ack received");
+  		if(!isValid(ack)){
+  			System.out.println("ack not valid");
+  		}else if (ack.getAck() > nextSequenceNo-1){
+  			
+  		}else if(ack.getAck() < nextSequenceNo-1){
+  			//assuming duplicate, older ack
+  			nextSequenceNo--;
+  			send(msg);
+  			return;
+  		}else{
+  			System.out.println("Valid ack received!");
+  		}
+  	}
+  }
 
 	/**
 	 * Wait for incoming data.
