5e9fb5db737b9071e80bf84636c28ac68f076015
Jens Kristian Espevik <jenskristian@gmail.com>
2013-04-22 12:17:00 +0200

    and I'm done

--- src/no/ntnu/fp/net/co/ConnectionImpl.java
+++ src/no/ntnu/fp/net/co/ConnectionImpl.java
@@ -7,13 +7,14 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import javax.print.attribute.standard.JobMessageFromOperator;
 
 import no.ntnu.fp.net.admin.Log;
 import no.ntnu.fp.net.cl.ClException;
@@ -30,46 +31,32 @@ import no.ntnu.fp.net.cl.KtnDatagram.Flag;
  * of the functionality, leaving message passing and error handling to this
  * implementation.
  * 
- * @author Sebjørn Birkeland and Stein Jakob Nordbø
+ * @author Sebj¿rn Birkeland and Stein Jakob Nordb¿
  * @see no.ntnu.fp.net.co.Connection
  * @see no.ntnu.fp.net.cl.ClSocket
  */
 public class ConnectionImpl extends AbstractConnection {
+	//Keeps track of the used ports for each server port
+	private static Map<Integer, Boolean> usedPorts = Collections
+			.synchronizedMap(new HashMap<Integer, Boolean>());
 
-	/** Keeps track of the used ports for each server port. */
-	private static Map<Integer, Boolean> usedPorts = Collections.synchronizedMap(new HashMap<Integer, Boolean>());
-	private ClSocket socket = new ClSocket();
-	private final int maxresends = 5;
-	private final int maxrereceives = 5;
-	private int resends = 0;
-	private int rereceives = 0;
-	private KtnDatagram lastDatagramReceived = null;
 	/**
-	 * Initialise initial sequence number and setup state machine.
+	 * Initializes initial sequence number and setup state machine.
 	 * 
 	 * @param myPort
 	 *            - the local port to associate with this connection
 	 */
 	public ConnectionImpl(int myPort) {
-		//        throw new NotImplementedException();
+		super(); //Initializes sequence number and sets state to disabled
+		myAddress = getIPv4Address();
 		this.myPort = myPort;
-		this.myAddress = getIPv4Address();
-		state = state.CLOSED; //there's no connection from the get-go
+		usedPorts.put(myPort, true);
 	}
-
-	public ConnectionImpl(String myAddress, int myPort, String remoteAddress, int remotePort){
-		this.myAddress = myAddress;
-		this.myPort = myPort;
-		this.remoteAddress = remoteAddress;
-		this.remotePort = remotePort;
-		state = state.SYN_RCVD;
-	}
-
-	private String getIPv4Address() {
+	
+	private static String getIPv4Address() {
 		try {
 			return InetAddress.getLocalHost().getHostAddress();
-		}
-		catch (UnknownHostException e) {
+		} catch (UnknownHostException e) {
 			return "127.0.0.1";
 		}
 	}
@@ -87,36 +74,66 @@ public class ConnectionImpl extends AbstractConnection {
 	 *             If timeout expires before connection is completed.
 	 * @see Connection#connect(InetAddress, int)
 	 */
-	public void connect(InetAddress remoteAddress, int remotePort) throws IOException,
-	SocketTimeoutException {
-		//        throw new NotImplementedException();
-
+	public void connect(InetAddress remoteAddress, int remotePort)
+			throws IOException, SocketTimeoutException {
 		this.remoteAddress = remoteAddress.getHostAddress();
 		this.remotePort = remotePort;
+		
+        if (state != State.CLOSED) {
+            throw new ConnectException("The socket is already connected");
+        }
+        Log.writeToLog("Trying to connect to: "
+							+ remoteAddress.getHostAddress() 
+							+ " : " 
+							+ remotePort, 
+							"ConnectionImpl");
+		//Handshake
+		try {
+			//Send SYN to server
+			int tries = 3;
+			boolean sent = false;
+			KtnDatagram synPacket = constructInternalPacket(Flag.SYN);
 
-		//We need a syn!
-		KtnDatagram syn = constructInternalPacket(Flag.SYN);
+			// Send the SYN, trying at most `tries' times.
+			Log.writeToLog(synPacket, "Sending SYN", "ConnectionImpl");
 
-		try{
-			simplySendPacket(syn);
-		}catch (ClException e){
-			e.printStackTrace();
+			do {
+				try {
+					new ClSocket().send(synPacket);
+					sent = true;
+				} catch (ConnectException e) {
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException ex) {/*Ignore*/}
+				}
+			} while (!sent && (tries-- > 0));
+
+			if (!sent) {
+				nextSequenceNo--;
+				throw new ConnectException("Unable to send SYN.");
+			}
+			state = State.SYN_SENT;
+			
+			//Receive SYNACK from server
+			KtnDatagram response = receiveAck();
+
+			//Connect to the new connection
+			this.remoteAddress = response.getSrc_addr();
+			this.remotePort = response.getSrc_port();
+			
+			//ACK the SYNACK
+			sendAck(response, false);
 		}
-
-		//Need to wait for synAck
-		KtnDatagram synAck = receiveAck();
-
-		if(synAck != null){
-			this.remotePort = synAck.getSrc_port();
-		} else {
-			throw new SocketTimeoutException();
+		catch (ConnectException e) {
+			throw e;
 		}
-		if(synAck.getFlag() == Flag.SYN_ACK){
-			state = State.ESTABLISHED;
+		catch (Exception e) {
+			throw new ConnectException("Could not establish connection with server");
 		}
-		sendAck(synAck, false);
+		state = State.ESTABLISHED;
+		Log.writeToLog("Connection established", "ConnectionImpl");
 	}
-
+	
 	/**
 	 * Listen for, and accept, incoming connections.
 	 * 
@@ -124,39 +141,44 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see Connection#accept()
 	 */
 	public Connection accept() throws IOException, SocketTimeoutException {
-		//        throw new NotImplementedException();
-
 		state = State.LISTEN;
-
-		KtnDatagram syn;
-		do{
-			syn = receivePacket(true);
-			System.out.println(syn);
-		} while (syn == null || syn.getFlag() != Flag.SYN);
-
-		this.remoteAddress = syn.getSrc_addr();
-		this.remotePort = syn.getSrc_port();
-		state = State.SYN_RCVD;
-
-		ConnectionImpl newConnection = new ConnectionImpl(this.myAddress, getNewPort(), this.remoteAddress, this.remotePort);
-
-		newConnection.sendAck(syn, true);
-
-		KtnDatagram ack = newConnection.receiveAck();
-
-		if(ack==null || ack.getFlag() != Flag.ACK){
-			throw new SocketTimeoutException();
+		KtnDatagram response = null;
+		Log.writeToLog("Listening for new connections on port " + myPort, 
+						"ConnectionImpl");
+		
+		while (!isValid(response) || response.getFlag() != Flag.SYN) {
+			//Wait for SYN package
+			response = receivePacket(true);
 		}
+		
+		//Set up the new connection
+		ConnectionImpl newConnection = new ConnectionImpl(getFreePort());
+		newConnection.lastValidPacketReceived = response;
+		newConnection.state = State.SYN_RCVD;
+		newConnection.remoteAddress = response.getSrc_addr();
+		newConnection.remotePort = response.getSrc_port();
 
+		//Send SYNACK
+		newConnection.sendAck(response, true);
+
+		newConnection.receiveAck();
+		
+		//Return the established connection to client
 		newConnection.state = State.ESTABLISHED;
-
-		state = State.LISTEN;
-
-		return (Connection) newConnection;
-	}
-
-	private int getNewPort(){
-		return (int) (Math.random()*40000+10000); //assuming the port we get is not in use!
+		state = State.CLOSED;
+		return newConnection;
+    }
+	
+	/**
+	 * @return An unused, random port between 10k and 20k.
+	 */
+	private static int getFreePort() {
+		int port;
+		
+		while (usedPorts.containsKey(
+					port = (int)(Math.random() * 30000) + 10000))//assuming it's not taken!
+			;
+		return port;
 	}
 
 	/**
@@ -171,40 +193,47 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see AbstractConnection#sendDataPacketWithRetransmit(KtnDatagram)
 	 * @see no.ntnu.fp.net.co.Connection#send(String)
 	 */
-	public void send(String msg) throws ConnectException, IOException {
-		//      throw new NotImplementedException();
-		if(state!= State.ESTABLISHED) throw new ConnectException("No connection established!");
-		KtnDatagram datagram = constructDataPacket(msg);
-
-		KtnDatagram ack = sendDataPacketWithRetransmit(datagram);//sends msg, waits for ack
-		System.out.println("send complete");
-		if(ack == null){
-			System.out.println("Where's the ack?");
-			if(resends < maxresends){
-				resends++;
-				send(msg);
-				resends = 0;
-				return;
-			} else {
-				state = State.CLOSED;
-				throw new ConnectException("Conncetion lost");
-
+	public synchronized void send(String msg) throws ConnectException, IOException {
+		if (state != State.ESTABLISHED) {
+			throw new ConnectException("Cannot send without an established connection");
+		}
+		KtnDatagram ack = null;
+		
+		while (ack == null) {
+			try {
+				ack = sendDataPacketWithRetransmit(constructDataPacket(msg));
+				
+				if (!isValid(ack)) {
+					//Bad checksum? Send again
+					ack = null;
+				}
 			}
+			catch (SocketException e) {/*Try again*/}
+		}
+	}
 
-		} else { //if ack showed up
-			System.out.println("ack received");
-			if(!isValid(ack)){
-				System.out.println("ack not valid");
-			}else if (ack.getAck() > nextSequenceNo-1){
+	private synchronized void sendFin() throws IOException {
+		int tries = 3;
+		boolean sent = false;
+		KtnDatagram finPacket = constructInternalPacket(Flag.FIN);
 
-			}else if(ack.getAck() < nextSequenceNo-1){
-				//assuming duplicate, older ack
-				nextSequenceNo--;
-				send(msg);
-				return;
-			}else{
-				System.out.println("Valid ack received!");
+		// Send the FIN, trying at most `tries' times.
+		Log.writeToLog(finPacket, "Sending FIN", "ConnectionImpl");
+
+		do {
+			try {
+				new ClSocket().send(finPacket);
+				sent = true;
+			} catch (Exception e) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException ex) {/*Ignore*/}
 			}
+		} while (!sent && (tries-- > 0));
+
+		if (!sent) {
+			nextSequenceNo--;
+			throw new ConnectException("Unable to send FIN.");
 		}
 	}
 
@@ -217,58 +246,30 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see AbstractConnection#sendAck(KtnDatagram, boolean)
 	 */
 	public String receive() throws ConnectException, IOException {
-
-		KtnDatagram datagram = null;
-		try{
-			datagram = receivePacket(false);
-		}catch (EOFException e){ // is that a FIN?
-			state = State.CLOSE_WAIT;
-			throw new EOFException();
+		if (state != State.ESTABLISHED) {
+			throw new ConnectException("No connection");
 		}
-		// Having a look at msg and acking if all is good
-		if(datagram == null){
-			if(rereceives < maxrereceives){
-				rereceives++;
-				String msg = receive();
-				rereceives = 0;
-				return msg;
+		KtnDatagram received = null;
 
-			}else{
-				state = State.CLOSED;
-				throw new ConnectException();
+		while (received == null) {
+			received = receivePacket(false);
+
+			if (lastValidPacketReceived != null && 
+					received.getSeq_nr() <= lastValidPacketReceived.getSeq_nr()) {
+				//Ignore the already received packet
+				received = null;
 			}
-		} else {
-			if(!isGhostPacket(datagram)){
-				System.out.println("Not a ghost!");
-				if(isValid(datagram)){
-					if(lastDatagramReceived != null && datagram.getSeq_nr()-1 != lastDatagramReceived.getSeq_nr()){
-						//is this the data we're waiting for?
-						System.out.println("1");
-						sendAck(lastDatagramReceived, false);
-						return receive();
-					}else{
-						System.out.println("2");
-						sendAck(datagram, false);
-						lastDatagramReceived = datagram;
-						return (String) datagram.getPayload();
-					}
-				}else{
-					if(lastDatagramReceived != null){
-						System.out.println("3");
-						sendAck(lastDatagramReceived, false); //requesting resend
-						return receive();
-					}
-					return receive();
-				}
-			}else {
-				System.out.println("it's a ghost!");
-				return receive();
+			else if (!isValid(received)) {
+				//Probably indicates a checksum mismatch
+				received = null;
 			}
+
 		}
+		//ACK the received packet
+		sendAck(received, false);
+		lastValidPacketReceived = received;
+		return (String)received.getPayload();
 	}
-
-
-
 
 
 	/**
@@ -277,75 +278,52 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @see Connection#close()
 	 */
 	public void close() throws IOException {
-		KtnDatagram ack; 
-		KtnDatagram datagram; 
-		KtnDatagram finAck = null;
-
-		if(state==State.CLOSE_WAIT){
-			sendAck(lastDatagramReceived, false);
-			try{
-				Thread.currentThread().sleep(1000);
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}
-			try{
-				datagram = constructInternalPacket(Flag.FIN);
-				simplySendPacket(datagram);
-			} catch(ClException e){
-				e.printStackTrace();
-			}
-			ack = receiveAck();
-			state = State.CLOSED;
+		if (state != State.ESTABLISHED) {
+			throw new ConnectException("Cannot disconnect!");
 		}
-		else if(state == State.ESTABLISHED){
-			try{
-				datagram = constructInternalPacket(Flag.FIN);
-				simplySendPacket(datagram);
-			} catch(ClException e){
-				e.printStackTrace();
-			}
-			state = State.FIN_WAIT_1;
-			ack = receiveAck();
-			if(ack == null){
-				if(resends < maxresends){
-					reclose();
-					return;
+
+		if (disconnectRequest == null) {
+			//Make disconnect request
+			try {
+				sendFin();
+				state = State.FIN_WAIT_1;
+
+				KtnDatagram response = receiveAck();
+				
+				if (response.getFlag() == Flag.FIN) {
+					disconnectRequest = response;
 				}
-				else state = State.CLOSED;
+				state = State.FIN_WAIT_2;
+
+				//Listen for FIN packet
+				KtnDatagram	finPacket = receivePacket(true);
+
+				if (finPacket.getFlag() != Flag.FIN) {
+					throw new Exception("Did not receive expected FIN from server.");
+				}
+
+				sendAck(finPacket, false);
+
+				//Give the server 10 seconds to close the connection
+				Thread.sleep(10000);
 			}
-			state = State.FIN_WAIT_2;
-			finAck = receiveAck();
-			if(finAck == null) finAck = receiveAck();
-			if(finAck != null) sendAck(finAck, false);
+			catch (Exception e) {}
 		}
+		else {
+			//Respond to disconnect request
+			try {
+				sendAck(disconnectRequest, false);
+				state = State.CLOSE_WAIT;
+				
+				sendFin();
+				state = State.LAST_ACK;
+				
+				receiveAck();
+			}
+			catch (Exception e) {}
+		}
+		//Close the connection on your side
 		state = State.CLOSED;
-	}
-
-
-
-
-
-
-	private void reclose() {
-		// TODO Auto-generated method stub
-
-		state = State.ESTABLISHED;
-
-		try{
-			close();
-		} catch(IOException e){
-			e.printStackTrace();
-		}
-
-	}
-
-	private boolean isGhostPacket(KtnDatagram datagram) {
-		// TODO Auto-generated method stub
-		if(datagram.getSrc_addr() != null){
-			return !(datagram.getSrc_addr().equals(remoteAddress) && datagram.getSrc_port()==remotePort);
-
-		}
-		return true;
 	}
 
 	/**
@@ -357,6 +335,25 @@ public class ConnectionImpl extends AbstractConnection {
 	 * @return true if packet is free of errors, false otherwise.
 	 */
 	protected boolean isValid(KtnDatagram packet) {
-		return packet.getChecksum() == packet.calculateChecksum();
+		if (packet == null) {
+			return false;
+		}
+
+		if (packet.calculateChecksum() != packet.getChecksum()) {
+			//Checksum mismatch
+			return false;
+		}
+		
+		if (packet.getFlag() == Flag.NONE && packet.getPayload() == null) {
+			//External packet with null-load is invalid
+			return false;
+		}
+		
+		if (packet.getFlag() != Flag.NONE && packet.getPayload() != null) {
+			//An internal packet with a non-null payload is invalid
+			return false;
+		}
+		//All tests passed
+		return true;
 	}
 }
